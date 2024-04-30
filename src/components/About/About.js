c089d618645caaee8882fba112d1c5b3b1f85fc7
Revan R <144509388+Revan2222@users.noreply.github.com>
2024-04-30 22:28:56 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,17 +10,17 @@ export const About = () => {
           <span className="purple-smoke-text">- INTRODUCTION</span>{" "}
         </span>
         <p className="gray-text">
-        Hey there, I'm Revan, an enthusiastic student with a passion for DevOps. I'm eager to learn about optimizing software development pipelines and collaborating effectively using GitHub. I love tackling challenges and exploring ways to make technology work smarter for us.{" "}
+          Hey there, I'm Revan, an enthusiastic student with a passion for DevOps. I'm eager to learn about optimizing software development pipelines and collaborating effectively using GitHub. I love tackling challenges and exploring ways to make technology work smarter for us.
         </p>
         <span className="white-text">
-          <a href="#projects">Portfolio</a>
+          <button>Portfolio</button>
         </span>
         <ul className="tech-stack">
           <li>Devops</li>
           <li>Ui/Ux Designer</li>
           <li>Full-stack Developer</li>
         </ul>
-        <a className="about-see" href="#">{"->"} </a>
+        <button className="about-see">Ver màs {"->"} </button>
       </div>
       <div className="about-right">
         <img src={iconImage} alt="" />
